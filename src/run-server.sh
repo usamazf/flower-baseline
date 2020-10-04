a895030ad948f4ed0eb5041a82d6c746f80c9177
usamazf <usama-zafar@outlook.com>
2020-10-04 15:19:57 +0800

    new server addresses on the cluster

--- src/run-server.sh
+++ src/run-server.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SERVER_ADDRESS="192.168.15.73:8888"
+SERVER_ADDRESS="12.12.12.101:8888" #"192.168.15.91:8888"
 
 # Start a Flower server
 python server.py \
     --server_address=$SERVER_ADDRESS \
     --rounds=5 \
     --sample_fraction=1.0 \
-    --min_sample_size=2 \
-    --min_num_clients=2
+    --min_sample_size=4 \
+    --min_num_clients=4
