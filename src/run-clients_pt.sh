873f2106adf651ec753c6370a19af5cadb8d47fe
Usama Zafar <usama-zafar@outlook.com>
2021-12-26 16:27:56 +0500

    Update run scripts

--- src/run-clients_pt.sh
+++ src/run-clients_pt.sh
@@ -3,10 +3,9 @@
 SERVER_ADDRESS="12.12.12.101:8888" #"192.168.15.91:8888"
 
 NUM_CLIENTS=2
-I_START=0
 
 echo "Starting $NUM_CLIENTS clients."
-for ((i = $I_START; i < $NUM_CLIENTS; i++))
+for ((i=0; i<$NUM_CLIENTS; i++))
 do
     echo "Starting client(cid=$i) with partition $i out of $NUM_CLIENTS clients."
     python client_pt.py \
