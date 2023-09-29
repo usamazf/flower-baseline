5674e5b32d28224c7745a0ac945571e6b6424ce2
Usama Zafar <usama-zafar@outlook.com>
2023-09-29 15:51:33 +0200

    bug removal, problem with torch.Tensor

--- src/models/resnet18_cifar.py
+++ src/models/resnet18_cifar.py
@@ -23,6 +23,6 @@ class Net(ResNet):
     def set_weights(self, weights: fl.common.NDArrays) -> None:
         """Set model weights from a list of NumPy ndarrays."""
         state_dict = OrderedDict(
-            {k: torch.Tensor(v) for k, v in zip(self.state_dict().keys(), weights)}
+            {k: torch.tensor(v) for k, v in zip(self.state_dict().keys(), weights)}
         )
         self.load_state_dict(state_dict, strict=True)
